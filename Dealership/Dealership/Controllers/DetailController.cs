ee7850e9fee564225ead04f6e7fde9984e3dcd6e
Nathan Foster <ngfost01@gmail.com>
2018-05-27 18:48:01 -0400

    Modified logic to enable Previous and Next

--- Dealership/Dealership/Controllers/DetailController.cs
+++ Dealership/Dealership/Controllers/DetailController.cs
@@ -19,5 +19,33 @@ namespace Dealership.Controllers
 
             return View(_carLot);
         }
+
+        public ActionResult Previous(int carIndex)
+        {
+            if (carIndex > 0)
+            {
+                ViewBag.CarIndex = carIndex - 1;
+            }
+            else
+            {
+                ViewBag.CarIndex = carIndex;
+            }
+
+            return View("Index", _carLot);
+        }
+
+        public ActionResult Next(int carIndex)
+        {
+            if (carIndex < _carLot.Cars.Count - 1)
+            {
+                ViewBag.CarIndex = carIndex + 1;
+            }
+            else
+            {
+                ViewBag.CarIndex = carIndex;
+            }
+
+            return View("Index", _carLot);
+        }
     }
 }
