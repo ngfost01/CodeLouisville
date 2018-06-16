3504017b7780580bbd933a941229d1118cf61866
Nathan Foster <ngfost01@gmail.com>
2018-06-16 19:44:27 -0400

    Initially Created

---
+++ ASPNETMVCEntityFramework/ASPNETMVCEntityFramework/EntityContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETMVCEntityFramework
+{
+    public class EntityContext:DbContext
+    {
+    }
+}
---
+++ ASPNETMVCEntityFramework/README.md
@@ -0,0 +1,7 @@
+# Code Louisville
+## ASP.NET MVC/Entity Framework Setup
+
+This project serves as a base for a project using ASP.NET MVC with Entity Framework.
+
+### Running
+Open project in Visual Studio and run.
